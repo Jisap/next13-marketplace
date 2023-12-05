ec6db3559a25c629e78558e9f658ec9685f78a04
Jorge Matos <jisapjisap@gmail.com>
2023-12-05 18:55:46 +0100

    Cart - FormatPrice

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -9,6 +9,7 @@ import { formatPrice } from "@/lib/utils";
 const Cart = () => {
 
   const itemCount = 1;
+  const fee = 1;
 
   return (
     <Sheet>
@@ -45,7 +46,7 @@ const Cart = () => {
                 </div>
                 <div className="flex">
                   <span className="flex-1">Transaction Fee</span>
-                  <span>{formatPrice(1)}</span>
+                  <span>{formatPrice(fee)}</span>
                 </div>
               </div>
             </div>
