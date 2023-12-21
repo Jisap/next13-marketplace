9ebeab6ef11b12cba92e3112b10f09c89cf8981c
Jorge Matos <jisapjisap@gmail.com>
2023-12-21 20:01:26 +0100

    Products - Media collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,6 +5,8 @@ import path from "path";
 import dotenv from 'dotenv';
 import { buildConfig } from "payload/config";
 import { Users } from "./collections/Users";
+import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
 
  dotenv.config({
    path: path.resolve(__dirname, '../.env'),
@@ -13,7 +15,7 @@ import { Users } from "./collections/Users";
 export default buildConfig({                                  // Configura el cms payload
 
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',        // Se define la URL del servidor que utilizará "Payload"
-  collections: [Users],                                       // Se definen las colecciones y rutas de la aplicación. 
+  collections: [Users, Products, Media],                      // Se definen las colecciones y rutas de la aplicación. 
   routes: {
     admin: '/sell'                                            // la interfaz de administración estará disponible en la ruta /sell.
   },
