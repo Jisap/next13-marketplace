6a95a3acaf1abed02f3923f93b1a6d1cc009721f
Jorge Matos <jisapjisap@gmail.com>
2024-01-03 18:14:53 +0100

    checkout

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,10 +2,13 @@
 
 import { PRODUCT_CATEGORIES } from "@/config"
 import { useCart } from "@/hooks/use-cart"
-import { cn } from "@/lib/utils"
+import { cn, formatPrice } from "@/lib/utils"
 import Image from "next/image"
 import { useEffect, useState } from "react"
 import { Product } from '../../payload-types';
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Check, X } from "lucide-react"
 
 const Page = () => {
 
@@ -57,6 +60,8 @@ const Page = () => {
                 const { image } = product.images[0];
                   return(
                     <li key={product.id} className="flex py-6 sm:py-10">
+                      
+                      {/* image */}
                       <div className="flex-shrink-0">
                         <div className="relative h-24 w-24">
                           { typeof image !== "string"  && image.url ? (
@@ -68,6 +73,56 @@ const Page = () => {
                             />
                           ): null}
                         </div>
+                      </div>
+
+                      {/* Details */}
+                      <div className='ml-4 flex flex-1 flex-col justify-between sm:ml-6'>
+                        <div className='relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0'>
+                          <div>
+                            <div className="flex justify-between">
+                              <h3 className='text-sm'>
+                                <Link
+                                  href={`/product/${product.id}`}
+                                  className='font-medium text-gray-700 hover:text-gray-800'>
+                                  {product.name}
+                                </Link>
+                              </h3>
+                            </div>
+
+                              <div className='mt-1 flex text-sm'>
+                                <p className='text-muted-foreground'>
+                                  Category: {label}
+                                </p>
+                              </div>
+
+                            <p className='mt-1 text-sm font-medium text-gray-900'>
+                              {formatPrice(product.price)}
+                            </p>
+                          </div>
+
+                          <div className='mt-4 sm:mt-0 sm:pr-9 w-20'>
+                            <div className='absolute right-0 top-0'>
+                              <Button
+                                aria-label='remove product'
+                                onClick={() =>
+                                  removeItem(product.id)
+                                }
+                                variant='ghost'>
+                                <X
+                                  className='h-5 w-5'
+                                  aria-hidden='true'
+                                />
+                              </Button>
+                            </div>
+                          </div>
+                        </div>
+
+                        <p className='mt-4 flex space-x-2 text-sm text-gray-700'>
+                          <Check className='h-5 w-5 flex-shrink-0 text-green-500' />
+                          <span>
+                            Eligible for instant delivery
+                          </span>
+                        </p>
                       </div>
                     </li>  
                   )
