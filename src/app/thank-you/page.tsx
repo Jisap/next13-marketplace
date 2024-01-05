fa705fb69ac9c533e92bf2f149acdb09941b2a17
Jorge Matos <jisapjisap@gmail.com>
2024-01-05 18:54:07 +0100

    thank-you page

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,5 +1,8 @@
+import { PRODUCT_CATEGORIES } from "@/config"
 import { getPayloadClient } from "@/get-payload"
 import { getServerSideUser } from "@/lib/payload-utils"
+import { formatPrice } from "@/lib/utils"
+import { Product, ProductFile } from "@/payload-types"
 import { cookies } from "next/headers"
 import Image from "next/image"
 import { notFound, redirect } from "next/navigation"
@@ -42,7 +45,11 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
     return redirect(`/sign-in?origin=thank-you?orderId=${order.id}`); // Si no coinciden, se redirige a la página de inicio de sesión con el parámetro origin en la URL.
   }
 
+  const products = order.products as Product[];   
 
+  const orderTotal = products.reduce((total, product) => {
+    return total + product.price
+  }, 0)
 
   return (
     <main className="relative lg:min-h-full">
@@ -82,6 +89,98 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
                 we&apos;ll send you confirmation very soon!
               </p>
             }
+
+            <div className='mt-16 text-sm font-medium'>
+              <div className='text-muted-foreground'>
+                Order nr.
+              </div>
+              <div className='mt-2 text-gray-900'>
+                {order.id}
+              </div>
+
+              <ul className='mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-muted-foreground'>
+                {(order.products as Product[]).map((product) => {
+                  
+                  const label = PRODUCT_CATEGORIES.find(
+                    ({ value }) =>
+                      value === product.category
+                  )?.label
+
+                  const downloadUrl = (product.product_files as ProductFile).url as string
+
+                  const { image } = product.images[0]
+
+                  return(
+                    <li
+                      key={product.id}
+                      className='flex space-x-6 py-6'
+                    >
+                      <div className='relative h-24 w-24'>
+                        {typeof image !== 'string' &&
+                          image.url ? (
+                          <Image
+                            fill
+                            src={image.url}
+                            alt={`${product.name} image`}
+                            className='flex-none rounded-md bg-gray-100 object-cover object-center'
+                          />
+                        ) : null}
+                      </div>
+                    
+                      <div className='flex-auto flex flex-col justify-between'>
+                        <div className='space-y-1'>
+                          <h3 className='text-gray-900'>
+                            {product.name}
+                          </h3>
+
+                          <p className='my-1'>
+                            Category: {label}
+                          </p>
+                        </div>
+
+                        {order._isPaid ? (
+                          <a
+                            href={downloadUrl}
+                            download={product.name}
+                            className='text-blue-600 hover:underline underline-offset-2'>
+                            Download asset
+                          </a>
+                        ) : null}
+                      </div>
+
+                      <p className='flex-none font-medium text-gray-900'>
+                        {formatPrice(product.price)}
+                      </p>
+
+                    </li>  
+                  )
+                })}
+              </ul>
+
+              <div className='space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-muted-foreground'>
+                <div className='flex justify-between'>
+                  <p>Subtotal</p>
+                  <p className='text-gray-900'>
+                    {formatPrice(orderTotal)}
+                  </p>
+                </div>
+
+                <div className='flex justify-between'>
+                  <p>Transaction Fee</p>
+                  <p className='text-gray-900'>
+                    {formatPrice(1)}
+                  </p>
+                </div>
+
+                <div className='flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900'>
+                  <p className='text-base'>Total</p>
+                  <p className='text-base'>
+                    {formatPrice(orderTotal + 1)}
+                  </p>
+                </div>
+              </div>
+
+            </div>
 
           </div>
         </div>
